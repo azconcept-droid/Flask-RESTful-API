8fc11f1571f6f60caa5706aac5bdaed4e5de1c97
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-12 18:39:34 +0100

    add other people's blog post

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -9,6 +9,23 @@ from app import app
 def index():
     """Default route, home page"""
 
+    # Mockup for users
     user = {'username': 'Azeez'}
 
-    return render_template('index.html', title='Home', user=user)
+    # Mockup for posts
+    posts = [
+        {
+            'author': {'username': 'James'},
+            'body': 'Beautiful day in Lagos'
+        },
+        {
+            'author': {'username': 'Ajoke'},
+            'body': 'The Avengers movies was so cool!'
+        },
+        {
+            'author': {'username': 'Usama'},
+            'body': 'Quran memo is beautiful'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
