53b1ce100951a545c83971c047b927f3905b41e4
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-13 13:06:45 +0100

    change url links to use url_for function

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,7 @@
 #!myvenv/bin/python3
 """Routes module for the Flask app"""
 
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -41,6 +41,6 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data
         ))
-        return redirect('/index')
+        return redirect(url_for('index'))
 
     return render_template('login.html', title='Sign In', form=form)
