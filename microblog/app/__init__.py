42f6750fc8048c18041ebf0afee0b7b9b4833b5e
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-13 12:44:41 +0100

    add login form feature

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,7 +2,9 @@
 """Initializes the Flask app"""
 
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
---
+++ microblog/config.py
@@ -0,0 +1,8 @@
+"""Config module"""
+
+import os
+
+class Config:
+    """ Configuration class """
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
